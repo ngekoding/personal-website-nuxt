da084e0edbe1bb3dd4e60a7933539dffda02ec70
Nur Muhammad <blog.nurmuhammad@gmail.com>
2023-02-18 22:35:03 +0800

    feat: add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   typescript: {
     shim: false,
-  }
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
