066be75d72e45856498752d009a4fcd85e1a338c
Nur Muhammad <blog.nurmuhammad@gmail.com>
2023-02-20 07:01:05 +0800

    refactor: use @nuxtjs/tailwindcss module instead

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,5 @@ export default defineNuxtConfig({
   typescript: {
     shim: false,
   },
-  css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
+  modules: ['@nuxtjs/tailwindcss']
 })
