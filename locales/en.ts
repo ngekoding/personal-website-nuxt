13c21889bf55303b7f6222a190d7b70b03e07ad9
Nur Muhammad <blog.nurmuhammad@gmail.com>
2023-12-13 06:29:34 +0800

    feat: change contact message - available for hire

--- locales/en.ts
+++ locales/en.ts
@@ -20,24 +20,24 @@ export default {
       intro: 'Hi, my name is',
       slogan: 'I help simplify your lives.',
       summary: 'I\'m a full-stack developer, my experience in both front-end '
-        + 'and back-end development allows me to build and maintain web '
-        + 'applications with a deep understanding of the user experience. '
-        + 'I enjoy solving complex problems and implementing innovative solutions.',
+      + 'and back-end development allows me to build and maintain web '
+      + 'applications with a deep understanding of the user experience. '
+      + 'I enjoy solving complex problems and implementing innovative solutions.',
     },
     about: {
       title: 'About Me',
       description: 'Hello! I\'m Nur Muhammad, and I\'m passionate about creating '
-        + 'innovative web-based solutions that make people\'s lives easier. '
-        + 'My love for web development began in 2012 when I first saw my senior '
-        + 'write HTML code with the &lt;marquee&gt; tag on the blackboard. '
-        + 'I was fascinated by how technology could be used to make things '
-        + 'happen on the internet.\n\n'
-        + 'Today, I spend my days writing code and creating projects both for '
-        + 'my job and personal endeavors. These projects allow me to explore '
-        + 'my creativity and improve my skills, all with the intention of '
-        + 'developing solutions that are relevant and valuable to others. '
-        + 'Ultimately, my goal is to make a positive impact by using '
-        + 'technology to enhance people\'s lives.',
+      + 'innovative web-based solutions that make people\'s lives easier. '
+      + 'My love for web development began in 2012 when I first saw my senior '
+      + 'write HTML code with the &lt;marquee&gt; tag on the blackboard. '
+      + 'I was fascinated by how technology could be used to make things '
+      + 'happen on the internet.\n\n'
+      + 'Today, I spend my days writing code and creating projects both for '
+      + 'my job and personal endeavors. These projects allow me to explore '
+      + 'my creativity and improve my skills, all with the intention of '
+      + 'developing solutions that are relevant and valuable to others. '
+      + 'Ultimately, my goal is to make a positive impact by using '
+      + 'technology to enhance people\'s lives.',
       usedTechnologiesTitle: 'Here are a few technologies I\'ve been working with:',
     },
     experience: {
@@ -45,33 +45,32 @@ export default {
       date: '2018 - 2022, July 2023 - Present (Full-Time) · April - June 2023 (Part-Time)',
       dateShort: '2018 - Present',
       description: 'Administer the full lifecycle of software development for '
-        + '20+ projects of the company with successful result while staying '
-        + 'limited resources. ',
+      + '20+ projects of the company with successful result while staying '
+      + 'limited resources. ',
     },
     work: {
       title: 'My\nWork',
       description: 'Utilizing a wide variety of programs and tools to provide '
-        + 'high-quality and cost-effective applications to boost organizational '
-        + 'efficiency and productivity.',
+      + 'high-quality and cost-effective applications to boost organizational '
+      + 'efficiency and productivity.',
     },
     contact: {
       title: 'Get in Touch!',
-      description: 'Hey, it\'s wonderful to hear from you! Although I\'m not actively '
-        + 'seeking new opportunities, my inbox is always open for you. '
-        + 'Don\'t hesitate to reach out if you have any questions or simply '
-        + 'want to say hi - I\'ll be more than happy to get back to you!',
+      description: 'I\'m seeking new opportunities, ready to craft digital '
+      + 'experiences that elevate efficiency and solve real-world challenges.\n\n'
+      + 'Let\'s connect and bring ideas to life!',
     },
   },
   recommendations: [
     {
       message: 'Nur Muhammad is the main backbone of the successful digital '
-        + 'transformation that has been carried out by the Faculty of Engineering '
-        + 'since 2018. Nur Muhammad\'s contribution to the implementation of '
-        + 'various digital services is very large, so that the Faculty of Engineering '
-        + 'UGM has succeeded in obtaining the WBK title from the Minister of '
-        + 'PANRB in 2021, which is 1 of 6 Faculties within the Ministry of '
-        + 'Education, Culture and Research, out of tens of thousands of '
-        + 'Faculties in Indonesia. Thank you Mas Nur.',
+      + 'transformation that has been carried out by the Faculty of Engineering '
+      + 'since 2018. Nur Muhammad\'s contribution to the implementation of '
+      + 'various digital services is very large, so that the Faculty of Engineering '
+      + 'UGM has succeeded in obtaining the WBK title from the Minister of '
+      + 'PANRB in 2021, which is 1 of 6 Faculties within the Ministry of '
+      + 'Education, Culture and Research, out of tens of thousands of '
+      + 'Faculties in Indonesia. Thank you Mas Nur.',
       author: {
         name: 'Prof. Ir. Selo, S.T., M.T., M.Sc., Ph.D., IPU, ASEAN Eng.',
         position: 'Dean of Faculty of Engineering, Universitas Gadjah Mada',
--- locales/id.ts
+++ locales/id.ts
@@ -20,25 +20,25 @@ export default {
       intro: 'Hi, nama saya',
       slogan: 'Mudahkan hidup Anda.',
       summary: 'Sebagai seorang full-stack developer, pengalaman saya pada bidang '
-        + 'front-end dan back-end memungkinkan saya untuk membuat aplikasi web '
-        + 'yang mudah dioperasikan. Saya sangat antusias untuk menyelesaikan '
-        + 'tantangan yang kompleks dan memberikan solusi inovatif.',
+      + 'front-end dan back-end memungkinkan saya untuk membuat aplikasi web '
+      + 'yang mudah dioperasikan. Saya sangat antusias untuk menyelesaikan '
+      + 'tantangan yang kompleks dan memberikan solusi inovatif.',
     },
     about: {
       title: 'Tentang Saya',
       description: 'Halo! Saya Nur Muhammad, dan saya sangat senang membuat '
-        + 'solusi inovatif berbasis web untuk membantu mempermudah hidup '
-        + 'orang lain. Saya memulai petualangan di dunia web pada tahun 2012, '
-        + 'yaitu ketika saya melihat kakak kelas saya menulis kode HTML dengan '
-        + 'tag &lt;marquee&gt; di papan tulis. Dan ketika melihat hasilnya, saya '
-        + 'merasa terheran-heran dan menganggap hebat sekali bisa membuat hal seperti itu.\n\n'
-        + 'Sekarang, saya mengisi hari-hari saya untuk menulis kode dan membuat '
-        + 'projek, baik untuk pekerjaan atau kebutuhan pribadi saya. Projek-projek '
-        + 'ini membantu untuk meningkatkan kreatifitas dan keterampilan saya, '
-        + 'yang mana semua hal itu untuk menciptakan solusi yang relevan dan '
-        + 'bermanfaat untuk orang lain. Pada akhirnya, tujuan saya adalah '
-        + 'memberikan dampak positif dengan teknologi untuk meningkatkan '
-        + 'kehidupan masyarakat.',
+      + 'solusi inovatif berbasis web untuk membantu mempermudah hidup '
+      + 'orang lain. Saya memulai petualangan di dunia web pada tahun 2012, '
+      + 'yaitu ketika saya melihat kakak kelas saya menulis kode HTML dengan '
+      + 'tag &lt;marquee&gt; di papan tulis. Dan ketika melihat hasilnya, saya '
+      + 'merasa terheran-heran dan menganggap hebat sekali bisa membuat hal seperti itu.\n\n'
+      + 'Sekarang, saya mengisi hari-hari saya untuk menulis kode dan membuat '
+      + 'projek, baik untuk pekerjaan atau kebutuhan pribadi saya. Projek-projek '
+      + 'ini membantu untuk meningkatkan kreatifitas dan keterampilan saya, '
+      + 'yang mana semua hal itu untuk menciptakan solusi yang relevan dan '
+      + 'bermanfaat untuk orang lain. Pada akhirnya, tujuan saya adalah '
+      + 'memberikan dampak positif dengan teknologi untuk meningkatkan '
+      + 'kehidupan masyarakat.',
       usedTechnologiesTitle: 'Berikut beberapa teknologi yang telah saya gunakan:',
     },
     experience: {
@@ -46,33 +46,31 @@ export default {
       date: '2018 - 2022, Juli 2023 - Sekarang (Purna Waktu) · April - Juni 2023 (Paruh Waktu)',
       dateShort: '2018 - Sekarang',
       description: 'Mengelola seluruh proses pengembangan aplikasi, yaitu lebih '
-        + 'dari 20 projek dengan hasil yang sukses walaupun dengan berbagai '
-        + 'keterbatasan sumber daya yang ada.',
+      + 'dari 20 projek dengan hasil yang sukses walaupun dengan berbagai '
+      + 'keterbatasan sumber daya yang ada.',
     },
     work: {
       title: 'Projek\nSaya',
       description: 'Memanfaatkan berbagai macam program dan alat bantu dalam '
-        + 'membuat aplikasi berkualitas tinggi dan hemat biaya untuk meningkatkan '
-        + 'efisiensi dan produktivitas perusahaan.',
+      + 'membuat aplikasi berkualitas tinggi dan hemat biaya untuk meningkatkan '
+      + 'efisiensi dan produktivitas perusahaan.',
     },
     contact: {
       title: 'Hubungi Saya',
-      description: 'Hai, senang sekali mendengar kabar dari Anda! Meskipun saya '
-        + 'sedang tidak aktif mencari peluang baru, tapi saya selalu terbuka '
-        + 'untuk menerima pesan Anda. Jangan ragu untuk menghubungi saya jika '
-        + 'Anda memiliki pertanyaan atau sekedar ingin menyapa, saya dengan '
-        + 'senang hati akan membalas.',
+      description: 'Saya sedang mencari peluang baru dan siap menciptakan '
+      + 'pengalaman digital yang meningkatkan efisiensi dan memecahkan tantangan '
+      + 'dunia nyata.\n\nMari terhubung dan wujudkan ide Anda!',
     },
   },
   recommendations: [
     {
       message: 'Nur Muhammad menjadi tulang punggung utama suksesnya transformasi '
-        + 'digital yg sudah dilakukan Fakultas Teknik sejak 2018. Kontribusi '
-        + 'Nur Muhammad dalam implementasi berbagai layanan digital sangat besar, '
-        + 'sehingga Fakultas Teknik UGM berhasil memperoleh predikat WBK dari '
-        + 'Menteri PANRB tahun 2021, yang merupakan 1 dari 6 Fakultas di '
-        + 'lingkungan KemenDikBudRistek, dari belasan ribu Fakultas di Indonesia. '
-        + 'Terima kasih Mas Nur.',
+      + 'digital yg sudah dilakukan Fakultas Teknik sejak 2018. Kontribusi '
+      + 'Nur Muhammad dalam implementasi berbagai layanan digital sangat besar, '
+      + 'sehingga Fakultas Teknik UGM berhasil memperoleh predikat WBK dari '
+      + 'Menteri PANRB tahun 2021, yang merupakan 1 dari 6 Fakultas di '
+      + 'lingkungan KemenDikBudRistek, dari belasan ribu Fakultas di Indonesia. '
+      + 'Terima kasih Mas Nur.',
       author: {
         name: 'Prof. Ir. Selo, S.T., M.T., M.Sc., Ph.D., IPU, ASEAN Eng.',
         position: 'Dekan Fakultas Teknik, Universitas Gadjah Mada',
@@ -84,12 +82,12 @@ export default {
     },
     {
       message: 'Saya bertemu dengan Nur saat ia bekerja sebagai senior developer '
-        + ' di Fakultas Teknik UGM. Saya bekerja sama dengannya secara langsung '
-        + 'untuk mengembangkan berbagai sistem informasi yang membuat '
-        + 'perusahaan kami lebih produktif. Nur memiliki komitmen yang tinggi '
-        + 'terhadap hasil kerja, pengalamannya dengan PHP dan LAMP stack membuatnya '
-        + 'sangat produktif dan memiliki kinerja yang baik. Terima kasih Nur, '
-        + 'semoga dalam waktu dekat kita bisa bekerja sama di projek lain.',
+      + ' di Fakultas Teknik UGM. Saya bekerja sama dengannya secara langsung '
+      + 'untuk mengembangkan berbagai sistem informasi yang membuat '
+      + 'perusahaan kami lebih produktif. Nur memiliki komitmen yang tinggi '
+      + 'terhadap hasil kerja, pengalamannya dengan PHP dan LAMP stack membuatnya '
+      + 'sangat produktif dan memiliki kinerja yang baik. Terima kasih Nur, '
+      + 'semoga dalam waktu dekat kita bisa bekerja sama di projek lain.',
       author: {
         name: 'Dr. Ir. Ridi Ferdiana, S.T., M.T., IPM.',
         position: 'Direktur Direktorat Sistem dan Sumber Daya Informasi, Universitas Gadjah Mada',
